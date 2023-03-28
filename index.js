09db2fec61b4ebbd9bf275d3bc5a25e61f66273f
krishnaPatelRb2 <93961739+KrishnaPatelRb@users.noreply.github.com>
2023-03-28 14:25:50 +0530

    push

--- index.js
+++ index.js
@@ -1,11 +1,144 @@
-import express from "express";
-const app = express();
-const port =9000;
+const express = require('express');
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config();
 
-app.use("/",(req,res)=>{
-    res.json({message:"hellow from express"})
-})
+const webApp = express();
 
-app.listen(9000,()=>{
-    console.log("server started on port 9000")
-})
+webApp.use(express.urlencoded({
+    extended: true
+}));
+webApp.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+const configuration = new Configuration({
+    apiKey: "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",
+    organization: "org-GX3JeMwj4zPW64TNjOOM9mLk"
+});
+const openai = new OpenAIApi(configuration);
+
+const textGeneration = async (prompt) => {
+
+    try {
+        const response = await openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: `Human: ${prompt}\nAI: `,
+            temperature: 0.9,
+            max_tokens: 500,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0.6,
+            stop: ['Human:', 'AI:']
+        });
+        console.log("Response", response.data.choices[0].text)
+
+        return {
+            status: 1,
+            response: `${response.data.choices[0].text}`
+        };
+    } catch (error) {
+        return {
+            status: 0,
+            response: ''
+        };
+    }
+};
+
+
+const formatResponseForDialogflow = (texts, sessionInfo, targetFlow, targetPage) => {
+
+    messages = []
+
+    texts.forEach(text => {
+        messages.push(
+            {
+                text: {
+                    text: [text],
+                    redactedText: [text]
+                },
+                responseType: 'HANDLER_PROMPT',
+                source: 'VIRTUAL_AGENT'
+            }
+        );
+    });
+
+    let responseData = {
+        fulfillment_response: {
+            messages: messages
+        }
+    };
+
+    if (sessionInfo !== '') {
+        responseData['sessionInfo'] = sessionInfo;
+    }
+
+    if (targetFlow !== '') {
+        responseData['targetFlow'] = targetFlow;
+    }
+
+    if (targetPage !== '') {
+        responseData['targetPage'] = targetPage;
+    }
+
+    return responseData
+};
+
+const getErrorMessage = () => {
+
+    return formatResponseForDialogflow(
+        [
+            'We are facing a technical issue.',
+            'Please try after sometimes or contact the XYZ restaurant.'
+        ],
+        '',
+        '',
+        ''
+    );
+};
+
+webApp.get('/', (req, res) => {
+    res.sendStatus(200);
+});
+
+webApp.post('/dialogflow', async (req, res) => {
+
+    let tag = req.body.fulfillmentInfo.tag;
+    let query = req.body.text;
+
+    console.log('A new request came...');
+    console.log(tag, "-" ,req.body.text);
+    console.log(new Date())
+
+    if (tag === 'sampleResponse') {
+        let result = await textGeneration(query);
+        if (result.status == 1) {
+            res.send(formatResponseForDialogflow(
+                [
+                    result.response
+                ],
+                '',
+                '',
+                ''
+            ));
+        } else {
+            res.send(getErrorMessage());
+        }
+
+    } else {
+        res.send(
+            formatResponseForDialogflow(
+                [
+                    'This is from the webhook.',
+                    'There is no tag set for this request.'
+                ],
+                '',
+                '',
+                ''
+            )
+        );
+    }
+});
+
+webApp.listen(PORT, () => {
+    console.log(`Server is up and running at ${PORT}`);
+});
